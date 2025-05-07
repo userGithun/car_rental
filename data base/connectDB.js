07495a2b50482debb015cd75217736f1ee67ae30
Harsh verma <chacha45454@gmail.com>
2025-05-07 14:11:35 +0530

    live

--- data base/connectDB.js
+++ data base/connectDB.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 const local_url = 'mongodb://127.0.0.1:27017/car_rental'
+const live_URL = 'mongodb+srv://chacha45454:ram123@cluster0.edlqo.mongodb.net/CarRental?retryWrites=true&w=majority&appName=Cluster0'
+
 
 const connectDB = ()=>{
-    return mongoose.connect(local_url)
+    return mongoose.connect(live_URL)
     .then(()=>{
         console.log('Data-base connected!')
     })
